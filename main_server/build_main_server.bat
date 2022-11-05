148e17402836017ce291ca6c1049cf6bf93be0c7
jankejc <jkornacki@o2.pl>
2022-11-05 11:14:36 +0100

    Make working images.

--- main_server/build_main_server.bat
+++ main_server/build_main_server.bat
@@ -1,2 +1,2 @@
 docker build -t apache_main .
-docker run -d --name server_main apache_main
+docker run -it --name server_main apache_main
--- secondary_server/build_client_server.bat
+++ secondary_server/build_client_server.bat
@@ -1,2 +1,2 @@
 docker build -t apache_secondary .
-docker run -d --name secondary_main apache_secondary
+docker run -it --name secondary_main apache_secondary
