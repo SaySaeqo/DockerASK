9889ff973c8219000b2acddcf5c62e42b452b544
jankejc <jkornacki@o2.pl>
2022-11-05 13:47:58 +0100

    Update in dockerfiles.
    Added new apt-gets.
    New container won't turn on automatically.

--- main_server/build_main_server.bat
+++ main_server/build_main_server.bat
@@ -1,2 +1,2 @@
 docker build -t apache_main .
-docker run --privileged -it -p 8080:8080 -p 8081:8081 --name server_main apache_main
+docker create --privileged -i -p 8080:8080 -p 8081:8081 --name server_main apache_main
